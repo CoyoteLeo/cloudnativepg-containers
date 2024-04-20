d63bc48586c04fee92e80b14ebcc903fdfb587d3
Leo Lin <karta09104@gmail.com>
2024-04-20 19:39:20 +0800

    build: add cd workflow to build images

---
+++ .github/generate-strategy.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+#
+# Given a list of PostgreSQL versions (defined as directories in the root
+# folder of the project), this script generates a JSON object that will be used
+# inside the Github workflows as a strategy to create a matrix of jobs to run.
+# The JSON object contains, for each PostgreSQL version, the tags of the
+# container image to be built.
+#
+set -eu
+
+# Define an optional aliases for some major versions
+declare -A aliases=(
+	[16]='latest'
+)
+
+GITHUB_ACTIONS=${GITHUB_ACTIONS:-false}
+
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}/..")")"
+BASE_DIRECTORY="$(pwd)"
+CLOUDNATIVEPG_DIRECTORY="${BASE_DIRECTORY}/postgres-containers"
+
+
+# Retrieve the PostgreSQL versions for Debian
+cd ${BASE_DIRECTORY}/Debian
+for version in */; do
+	[[ $version == src/ ]] && continue
+	debian_versions+=("$version")
+done
+debian_versions=("${debian_versions[@]%/}")
+
+# Sort the version numbers with highest first
+mapfile -t debian_versions < <(IFS=$'\n'; sort -rV <<< "${debian_versions[*]}")
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"
+	shift
+	local out
+	printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+echo "debian_versions=${debian_versions}"
+
+entries=()
+for version in "${debian_versions[@]}"; do
+
+	# Read versions from the definition file
+	versionFile="${CLOUDNATIVEPG_DIRECTORY}/Debian/${version}/.versions.json"
+	postgresImageVersion=$(jq -r '.POSTGRES_IMAGE_VERSION | split("-") | .[0]' "${versionFile}")
+	releaseVersion=$(jq -r '.IMAGE_RELEASE_VERSION' "${versionFile}")
+
+	# Initial aliases are "major version", "optional alias", "full version with release"
+	# i.e. "14", "latest", "14.2-1", "14.2-debian","14.2"
+	versionAliases=(
+			"${version}"
+			${aliases[$version]:+"${aliases[$version]}"}
+			"${postgresImageVersion}-${releaseVersion}"
+			"${postgresImageVersion}"
+		)
+	# Add all the version prefixes between full version and major version
+	# i.e "13.2"
+	while [ "$postgresImageVersion" != "$version" ] && [ "${postgresImageVersion%[.-]*}" != "$postgresImageVersion" ]; do
+		versionAliases+=("$postgresImageVersion-debian")
+		postgresImageVersion="${postgresImageVersion%[.-]*}"
+	done
+    # Support platform for container images
+	platforms="linux/amd64,linux/arm64"
+
+	# Build the json entry
+	entries+=(
+		"{\"name\": \"Debian ${postgresImageVersion}\", \"platforms\": \"$platforms\", \"dir\": \"Debian/$version\", \"file\": \"Debian/$version/Dockerfile\", \"version\": \"$version\", \"tags\": [\"$(join "\", \"" "${versionAliases[@]}")\"]}"
+	)
+done
+
+# Build the strategy as a JSON object
+strategy="{\"fail-fast\": false, \"matrix\": {\"include\": [$(join ', ' "${entries[@]}")]}}"
+jq -C . <<<"$strategy" # sanity check / debugging aid
+
+if [[ "$GITHUB_ACTIONS" == "true" ]]; then
+	echo "strategy=$(jq -c . <<<"$strategy")" >> $GITHUB_OUTPUT
+fi
---
+++ README.md
@@ -0,0 +1,34 @@
+# PostgreSQL with Multiple Extensions Docker Image
+
+> Reference: [cloudnative-pg/postgres-containers](https://github.com/cloudnative-pg/postgres-containers)
+
+This Docker image is based on the official [CloudNative](https://github.com/cloudnative-pg/cloudnative-pg) PostgreSQL image (ghcr.io/cloudnative-pg/postgresql) using Debian as the base OS.
+
+- The official image include the following package:
+  - Barman Cloud
+  - PGAudit
+  - Postgres Failover Slots
+  - pgvector
+- The image of this repository adds the following packages:
+  - TimescaleDB
+  - PGroonga
+
+## Getting Started
+
+To use this Docker image, you must have Docker installed on your machine. If you do not have Docker installed, follow the instructions on the [official Docker documentation](https://docs.docker.com/get-docker/) to get started.
+
+### Pulling the Image
+
+This image isn't hosted on a public registry but can be built directly using the provided Dockerfile.
+
+### Building the Image
+
+To build the image:
+
+1. Save the Dockerfile to a directory on your computer.
+2. Open a terminal and navigate to the directory containing the Dockerfile.
+3. Run the following command to build the image:
+   ```bash
+   docker build -t pg
+   ```
+
